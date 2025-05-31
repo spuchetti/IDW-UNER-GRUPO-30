4661e9f9e5b0fda150c3daf26502d7ad6069a4fa
tomygl1234 <tomasgimenezlascano123@gmail.com>
2025-06-01 00:30:02 +0200

    Con Seba estuvimos haciendo el crud y validaciones varias para darle forma al proyecto, queda mucho por corregir y ver que no hayan errores como en la entrega pasada

---
+++ src/js/login.js
@@ -0,0 +1,30 @@
+import { login } from './auth.js';
+
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const recordarCheckbox = document.getElementById('checkbox_recordar');
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  emailInput.value = "admin@idwsa.com";
+  passwordInput.value = "admin123";
+  recordarCheckbox.checked = true;
+});
+
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (recordarCheckbox.checked) {
+    localStorage.setItem('loginRecordado', JSON.stringify({ email, password }));
+  } else {
+    localStorage.removeItem('loginRecordado');
+  }
+
+  if (login(email, password)) {
+    window.location.href = "cpanel.html";
+  } else {
+    alert("Usuario o contraseña incorrectos");
+  }
+});
